95d7235465b2a1d60ff6c4acbb62b1a760f9787c
Daniel Souza <workdndsza@gmail.com>
2024-07-29 16:40:02 -0300

    refatorando

--- src/main/java/com/dnsouzadev/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/dnsouzadev/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,11 @@
 package com.dnsouzadev.screenmatch;
 
-import com.dnsouzadev.screenmatch.model.DadosEpisodio;
-import com.dnsouzadev.screenmatch.model.DadosSerie;
-import com.dnsouzadev.screenmatch.model.DadosTemporada;
-import com.dnsouzadev.screenmatch.service.ConsumoAPI;
-import com.dnsouzadev.screenmatch.service.ConverteDados;
+import com.dnsouzadev.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,27 +16,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
 
-		var json = consumoAPI.obterDados("https://omdbapi.com/?t=supernatural&season=15&apikey=99277d5");
-		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		json = consumoAPI.obterDados("https://omdbapi.com/?t=supernatural&season=15&episode=4&apikey=99277d5");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 1; i <= dados.totalTemporadas(); i++) {
-			json = consumoAPI.obterDados("https://omdbapi.com/?t=supernatural&season=" + i + "&apikey=99277d5");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
---
+++ src/main/java/com/dnsouzadev/screenmatch/principal/Principal.java
@@ -0,0 +1,45 @@
+package com.dnsouzadev.screenmatch.principal;
+
+import com.dnsouzadev.screenmatch.model.DadosSerie;
+import com.dnsouzadev.screenmatch.service.ConsumoAPI;
+import com.dnsouzadev.screenmatch.service.ConverteDados;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.ArrayList;
+import java.util.List;
+import com.dnsouzadev.screenmatch.service.ConsumoAPI;
+import com.dnsouzadev.screenmatch.service.ConverteDados;
+import com.dnsouzadev.screenmatch.model.DadosEpisodio;
+import com.dnsouzadev.screenmatch.model.DadosSerie;
+import com.dnsouzadev.screenmatch.model.DadosTemporada;
+import java.util.Scanner;
+
+public class Principal {
+    private final Scanner leitura = new Scanner(System.in);
+    private final ConsumoAPI consumo = new ConsumoAPI();
+    private final ConverteDados conversor = new ConverteDados();
+
+    private final String ENDERECO = "https://omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=99277d5";
+
+    public void exibeMenu() throws JsonProcessingException {
+        System.out.println("Digite o nome da serie: ");
+        String nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY + "&season=" + i);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+    }
+}
