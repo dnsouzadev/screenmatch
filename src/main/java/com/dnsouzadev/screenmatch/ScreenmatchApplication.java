f1c1cfb208545ba1e1d8b0bf3371fe500d4c9fb4
Daniel Souza <workdndsza@gmail.com>
2024-07-29 14:35:53 -0300

    criando o consumoApi

--- src/main/java/com/dnsouzadev/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/dnsouzadev/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package com.dnsouzadev.screenmatch;
 
+import com.dnsouzadev.screenmatch.service.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World!");
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://omdbapi.com/?t=supernatural&season=15&apikey=99277d5");
+		System.out.println(json);
 	}
 }
